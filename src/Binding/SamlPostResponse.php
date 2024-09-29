6650ae900228755167932e375d0593c09d7bc6b3
ricardo <ricardo@rayvenues.com>
2024-09-29 14:19:32 -0400

    change title

--- src/Binding/SamlPostResponse.php
+++ src/Binding/SamlPostResponse.php
@@ -49,7 +49,7 @@ class SamlPostResponse extends Response
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>POST data</title>
+    <title>Processing...</title>
 </head>
 <body onload="document.getElementById('a-very-unique-input-id#lightSAML').click();">
 
