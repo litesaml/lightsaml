183884fae3eb2fe251e61df55943e87035ab1339
Jeff Farthing <jeff@jfarthing.com>
2025-08-27 21:59:41 +0000

    Remove abstract declaration from Signature class

--- src/Model/XmlDSig/Signature.php
+++ src/Model/XmlDSig/Signature.php
@@ -2,9 +2,12 @@
 
 namespace LightSaml\Model\XmlDSig;
 
+use DOMNode;
 use LightSaml\Model\AbstractSamlModel;
+use LightSaml\Model\Context\DeserializationContext;
+use LightSaml\Model\Context\SerializationContext;
 
-abstract class Signature extends AbstractSamlModel
+class Signature extends AbstractSamlModel
 {
     /**
      * @return string
@@ -12,5 +15,21 @@ abstract class Signature extends AbstractSamlModel
     protected function getIDName()
     {
         return 'ID';
+    }
+
+    /**
+     * @return void
+     */
+    public function serialize(DOMNode $parent, SerializationContext $context)
+    {
+        //
+    }
+
+    /**
+     * @return void
+     */
+    public function deserialize(DOMNode $node, DeserializationContext $context)
+    {
+        //
     }
 }
